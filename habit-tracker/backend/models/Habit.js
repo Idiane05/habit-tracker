920f1622e52d5475e18c8fd24f189b2327bd6f0f
Diane INGABIRE <127386345+Idiane05@users.noreply.github.com>
2025-08-05 00:41:02 +0200

    debug'

--- habit-tracker/backend/models/Habit.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const habitSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  done: { type: Boolean, default: false }
-});
-
-module.exports = mongoose.model('Habit', habitSchema);
